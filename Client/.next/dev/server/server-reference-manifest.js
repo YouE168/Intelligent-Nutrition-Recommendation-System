43f5a4e56f9a752a8ee7727e40cc0b53b2f1f37a
Varun Harinath <varunharinath15@gmail.com>
2025-12-04 13:20:56 -0600

    Stop tracking .next folder

--- Client/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"K2ZEBj8NwMfdxb2cXpKQVncZr36DtKD5aXg9LwcFAtE=\"\n}"
--- Client/.next/dev/static/chunks/app_ai-chat_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_2e75c008._.js",
-  "static/chunks/_27eb1c46._.js"
-],
-    source: "dynamic"
-});
